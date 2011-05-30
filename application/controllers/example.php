a4d6b9ce43f1d7658ca5b341e0347fde9d93f74d
Alex <alex_wareing@aiwmedia.com>
2011-05-30 05:03:36 +0100

    Initial Add

---
+++ application/controllers/example.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Example extends CI_Controller {
+
+	public function index()
+    {
+    
+	    //Load the library
+	    $this->load->library('html2pdf');
+	    
+	    //Set folder to save PDF to
+	    $this->html2pdf->folder('./assets/pdfs/');
+	    
+	    //Set the filename to save/download as
+	    $this->html2pdf->filename('test.pdf');
+	    
+	    //Set the paper defaults
+	    $this->html2pdf->paper('a4', 'portrait');
+	    
+	    $data = array(
+	    	'title' => 'PDF Created',
+	    	'message' => 'Hello World!'
+	    );
+	    
+	    //Load html view
+	    $this->html2pdf->html($this->load->view('pdf', $data, true));
+	    
+	    if($this->html2pdf->create('save')) {
+	    	//PDF was successfully saved or downloaded
+	    	echo 'PDF saved';
+	    }
+	    
+    } 
+}
+
+/* End of file example.php */
+/* Location: ./application/controllers/example.php */
---
+++ application/libraries/html2pdf.php
@@ -0,0 +1,163 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class Html2pdf {
+
+    var $html;
+    var $path;
+    var $filename;
+    var $paper_size;
+    var $orientation;
+    
+    /**
+     * Constructor
+     *
+     * @access	public
+     * @param	array	initialization parameters
+     */	
+    function Html2pdf($params = array())
+    {
+        $this->CI =& get_instance();
+        
+        if (count($params) > 0)
+        {
+            $this->initialize($params);
+        }
+    	
+        log_message('debug', 'PDF Class Initialized');
+    
+    }
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Initialize Preferences
+	 *
+	 * @access	public
+	 * @param	array	initialization parameters
+	 * @return	void
+	 */	
+    function initialize($params)
+	{
+        $this->clear();
+		if (count($params) > 0)
+        {
+            foreach ($params as $key => $value)
+            {
+                if (isset($this->$key))
+                {
+                    $this->$key = $value;
+                }
+            }
+        }
+	}
+	
+	// --------------------------------------------------------------------
+
+	/**
+	 * Set html
+	 *
+	 * @access	public
+	 * @return	void
+	 */	
+	function html($html = NULL)
+	{
+        $this->html = $html;
+	}
+	
+	// --------------------------------------------------------------------
+
+	/**
+	 * Set path
+	 *
+	 * @access	public
+	 * @return	void
+	 */	
+	function folder($path)
+	{
+        $this->path = $path;
+	}
+	
+	// --------------------------------------------------------------------
+
+	/**
+	 * Set path
+	 *
+	 * @access	public
+	 * @return	void
+	 */	
+	function filename($filename)
+	{
+        $this->filename = $filename;
+	}
+	
+	// --------------------------------------------------------------------
+
+
+	/**
+	 * Set paper
+	 *
+	 * @access	public
+	 * @return	void
+	 */	
+	function paper($paper_size = NULL, $orientation = NULL)
+	{
+        $this->paper_size = $paper_size;
+        $this->orientation = $orientation;
+	}
+	
+	// --------------------------------------------------------------------
+
+
+	/**
+	 * Create PDF
+	 *
+	 * @access	public
+	 * @return	void
+	 */	
+	function create($mode = 'download') 
+	{
+	    
+   		if (is_null($this->html)) {
+			show_error("HTML is not set");
+		}
+	    
+   		if (is_null($this->path)) {
+			show_error("Path is not set");
+		}
+	    
+   		if (is_null($this->paper_size)) {
+			show_error("Paper size not set");
+		}
+		
+		if (is_null($this->orientation)) {
+			show_error("Orientation not set");
+		}
+	    
+	    //Load the DOMPDF libary
+	    require_once("dompdf/dompdf_config.inc.php");
+	    
+	    $dompdf = new DOMPDF();
+	    $dompdf->load_html($this->html);
+	    $dompdf->set_paper($this->paper_size, $this->orientation);
+	    $dompdf->render();
+	    
+	    if($mode == 'save') {
+    	    $this->CI->load->helper('file');
+		    if(write_file($this->path.$this->filename, $dompdf->output())) {
+		    	return TRUE;
+		    } else {
+				show_error("PDF could not be written to the path");
+		    }
+		} else {
+			
+			if($dompdf->stream($this->filename)) {
+				return TRUE;
+			} else {
+				show_error("PDF could not be streamed");
+			}
+	    }
+	}
+	
+}
+
+/* End of file Html2pdf.php */
